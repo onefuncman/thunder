17887e3dc223f8ebbf3f78d42cae508ace35ed12
Kami <kami@hearthworld.com>
2026-04-15 15:34:05 +0200

    zoom out doesn't clip that early.

--- src/haven/MapView.java
+++ src/haven/MapView.java
@@ -118,9 +118,9 @@ public class MapView extends PView implements DTarget, Console.Directory, Widget
 	public void resized() {
 	    float field = 0.5f;
 	    float aspect = ((float)sz.y) / ((float)sz.x);
-	    proj = Projection.frustum(-field, field, -aspect * field, aspect * field, 1, 2000);
+	    proj = Projection.frustum(-field, field, -aspect * field, aspect * field, 1, 5000);
 	}
-	
+
 	public void apply(Pipe p) {
 	    proj.apply(p);
 	    view.apply(p);
@@ -249,7 +249,7 @@ public class MapView extends PView implements DTarget, Console.Directory, Widget
 	    
 	    float field = field(elev);
 	    view = haven.render.Camera.pointed(curc.add(camoff).add(0.0f, 0.0f, h), dist(elev), elev, angl);
-	    proj = Projection.frustum(-field, field, -ca * field, ca * field, 1, 2000);
+	    proj = Projection.frustum(-field, field, -ca * field, ca * field, 1, 5000);
 	}
 	
 	public float angle() {
